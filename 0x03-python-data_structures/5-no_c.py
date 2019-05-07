b4cbe4cafb56a7e18f83971969d0716f943fb5c3
Heimer <732@holbertonschool.com>
2019-05-07 18:01:44 +0000

    task 5 correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     if my_string is not None:
         localstr = my_string.translate({ord('c'): None})
-        localstr = my_string.translate({ord('C'): None})
+        localstr = localstr.translate({ord('C'): None})
         return localstr
