35a38f3e5492e81666be440b9608e03c552718dc
Heimer <732@holbertonschool.com>
2019-05-07 17:45:04 +0000

    task 4 pep8 ajustment

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is not None
+    if my_list is not None:
         local = my_list.copy()
         if idx < 0 or idx > len(my_list):
             return local
         else:
             local[idx] = element
-            return local
+        return local
