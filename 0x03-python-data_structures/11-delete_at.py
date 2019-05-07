399390f21a1aee7b6809a73fde568243abc1bb0c
Heimer <732@holbertonschool.com>
2019-05-07 21:54:46 +0000

    task 11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+def delete_at(my_list=[], idx=0):
+    if my_list is not None:
+        if idx < 0 or idx >= len(my_list):
+            return my_list
+        else:
+            del my_list[idx:idx+1]
+            return my_list
