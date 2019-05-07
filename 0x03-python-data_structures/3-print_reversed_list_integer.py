9ad6a9cc83bf7b82fd02783bc7b799d8daa762d7
Heimer <732@holbertonschool.com>
2019-05-07 17:41:08 +0000

    task none condition added

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    local = my_list.copy()
-    local.reverse()
-    for i in range(len(local)):
-        print("{:d}".format(local[i]))
+    if my_list is not None:
+        local = my_list.copy()
+        local.reverse()
+        for i in range(len(local)):
+            print("{:d}".format(local[i]))
