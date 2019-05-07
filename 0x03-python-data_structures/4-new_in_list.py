6917011c77c2665c995878f06ecd497638474001
Heimer <732@holbertonschool.com>
2019-05-07 17:31:13 +0000

    task 4 fixed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 def new_in_list(my_list, idx, element):
     local = my_list.copy()
     if idx < 0 or idx > len(my_list):
-        return None
+        return my_list
     else:
         local[idx] = element
         return local
