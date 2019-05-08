81238dd429b1d89a825ca8cee4fecbff2f7fbd08
Heimer <732@holbertonschool.com>
2019-05-08 02:22:23 +0000

    task 100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * print_python_list_info - prints some basic info about Python lists.
+ * @p: pointer to a object
+ * Return: no return
+*/
+void print_python_list_info(PyObject *p)
+{
+  PyList_Size
+
+
+}
