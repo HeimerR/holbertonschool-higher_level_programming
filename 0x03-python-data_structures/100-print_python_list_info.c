1895a35d2953210ce8b14031000645b4c649b6d0
HeimerR <ing.heimer.rojas@gmail.com>
2019-05-08 05:06:30 +0000

    task 13

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,13 +1,14 @@
+#include <Python.h>
 #include <stdio.h>
-#include <stdlib.h>
-/*
+/**
  * print_python_list_info - prints some basic info about Python lists.
  * @p: pointer to a object
  * Return: no return
-*/
+**/
 void print_python_list_info(PyObject *p)
 {
-  PyList_Size
+	int sizeobj;
 
-
+	sizeobj = PyList_Size(p);
+	printf("[*] Size of the Python List = %d\n", sizeobj);
 }
