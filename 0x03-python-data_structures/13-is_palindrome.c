62f25c37182fc3a3f0e8ae2914a61b22b88dd6ea
Heimer <732@holbertonschool.com>
2019-05-08 00:24:01 +0000

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+* is_palindrome - frees a listint_t list
+* @head: pointer to list to be freed
+* Return: 1 if it is palidrome, 0 if it is not
+*/
+int is_palindrome(listint_t **head)
+{
+	int len = 0, len2 = 0;
+	listint_t *h, *h2;
+
+	h = *head;
+	if (!h)
+		return (1);
+	while (h)
+	{
+		h = h->next;
+		len2++;
+	}
+	h = *head;
+	while (len2 > 0)
+	{
+		h2 = h;
+		len = len2;
+		while (len - 1)
+		{
+			h2 = h2->next;
+			len--;
+		}
+		if (h->n != h2->n)
+			return (0);
+		len2 -= 2;
+		h = h->next;
+	}
+	return (1);
+}
