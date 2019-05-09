c993cc88530d9775328de9fa856fd0e5f151c179
Heimer Rojas <732@holbertonschool.com>
2019-05-09 21:15:27 +0000

    task 10 nonen conditions

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary is not None and len(a_dictionary) > 0:
         valist = list(sorted(a_dictionary.values()))
         num = valist[0]
         for k, v in sorted(a_dictionary.items()):
@@ -8,3 +8,5 @@ def best_score(a_dictionary):
                 num = v
                 max_score = k
         return max_score
+    else:
+        return None
