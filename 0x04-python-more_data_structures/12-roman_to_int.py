78c8773ce0d17707bc0b7f0c599d9ef7fd4e43d4
Heimer Rojas <732@holbertonschool.com>
2019-05-09 23:18:27 +0000

    task 12 none condition

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
     symbols = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     roman_list = list(roman_string)
     num = 0
     flag = 1
-    for i in range(len(roman_string) - 1):
+    i = 0
+    while i < (len(roman_string) - 1):
         if symbols[roman_list[i]] < symbols[roman_list[i + 1]]:
             num += symbols[roman_list[i + 1]] - symbols[roman_list[i]]
-            i = i + 1
+            i += 2
             flag = 0
         else:
             num += symbols[roman_list[i]]
             flag = 1
+            i += 1
     if flag == 1:
         num += symbols[roman_list[-1]]
     return num
