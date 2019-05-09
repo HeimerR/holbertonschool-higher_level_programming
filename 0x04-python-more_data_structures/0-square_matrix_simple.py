3ecef441c5c473ca7490a1877cb80b300b15127d
Heimer Rojas <732@holbertonschool.com>
2019-05-09 13:55:25 +0000

    task 0 none condition

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[row[i]**2 for i in range(len(matrix[0]))] for row in matrix]
+    if matrix is not None:
+        return [[row[i]**2 for i in range(len(matrix[0]))] for row in matrix]
