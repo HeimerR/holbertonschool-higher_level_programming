af78a59a9346c4db9c441275e0a443d5076884aa
Heimer Rojas <732@holbertonschool.com>
2019-05-09 19:52:06 +0000

    task 9 if none condition

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    aux = a_dictionary.copy()
-    for item in aux:
-        val = aux[item] * 2
-        tuple_aux = {item: val}
-        aux.update(tuple_aux)
-    return aux
+    if a_dictionary is not None:
+        aux = a_dictionary.copy()
+        for item in aux:
+            val = aux[item] * 2
+            tuple_aux = {item: val}
+            aux.update(tuple_aux)
+        return aux
