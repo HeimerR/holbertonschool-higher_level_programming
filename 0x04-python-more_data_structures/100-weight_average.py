cbfbad02de324cdfc816b6a6e447dc73ee717941
Heimer Rojas <732@holbertonschool.com>
2019-05-10 00:02:22 +0000

    task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list is []:
+        return 0
+    num = 0
+    den = 0
+    for i in range(len(my_list)):
+        num += my_list[i][0] * my_list[i][1]
+        den += my_list[i][1]
+    return num/den
