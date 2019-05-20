3ed2741a3591bfb4511700928a9f25eeedec9350
HeimerR <732@holbertonschool.com>
2019-05-20 17:04:59 +0000

    task 0 exception general

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end='')
-    except IndexError:
+    except Exception:
         print()
         return i
     else:
