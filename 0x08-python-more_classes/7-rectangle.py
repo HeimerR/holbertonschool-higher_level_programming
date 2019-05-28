38f2aaec054d8909374c6eb75980182f443e802e
HeimerR <732@holbertonschool.com>
2019-05-28 01:28:56 +0000

    comments added

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -16,14 +16,20 @@ class Rectangle:
 
     @property
     def width(self):
+        """ width
+        """
         return self.__width
 
     @property
     def height(self):
+        """ height
+        """
         return self.__height
 
     @width.setter
     def width(self, value):
+        """ width setter
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -32,6 +38,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        """ height setter
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
