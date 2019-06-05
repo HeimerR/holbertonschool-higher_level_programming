dbb5064cdebc1dd54b2796090bdb6cc3dc52b13b
Heimer <732@holbertonschool.com>
2019-06-05 22:26:56 +0000

    task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" module
+"""
+
+
+class Student:
+    """ class student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (type(attrs) is list and
+           all(list(map(lambda x: type(x) == str, attrs)))):
+            my_dict = {}
+            for attr in attrs:
+                if attr in self.__dict__.keys():
+                    value = getattr(self, attr)
+                    my_dict.update({attr: value})
+            return my_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for k, v in sorted(json.items()):
+            self.__dict__[k] = v
