8ffe4a0130dc02a97fa6665d7016ee4191f0d3f6
HeimerR <ing.heimer.rojas@gmail.com>
2019-06-08 23:21:19 +0000

    mandatories done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ Module base
 import json
 
 
+
 class Base:
     """class"""
     __nb_objects = 0
@@ -16,6 +17,7 @@ class Base:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or len(list_dictionaries) is 0:
             return []
@@ -23,4 +25,30 @@ class Base:
    
     @classmethod
     def save_to_file(cls, list_objs):
-        print(cls.__name__)
+        with open(cls.__name__ + ".json", encoding="utf-8", mode="w") as j_file:
+            if list_objs != None:
+                list_dict = [item.to_dictionary() for item in list_objs]
+                j_file.write(cls.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None :
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        dummy = cls(1, 1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        try:
+            with open(cls.__name__ + ".json", encoding="utf-8") as j_file:
+                list_file = cls.from_json_string(j_file.read())
+                return [cls.create(**obj) for obj in list_file]
+
+        except IOError:
+            return []
+
