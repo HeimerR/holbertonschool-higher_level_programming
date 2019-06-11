0eb09ee9fe61651123b1b2819f59dc098ac17591
Heimer <732@holbertonschool.com>
2019-06-11 20:04:45 +0000

    comments and ==

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,7 +22,8 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None or len(list_dictionaries) is 0:
+        """ serialization """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
