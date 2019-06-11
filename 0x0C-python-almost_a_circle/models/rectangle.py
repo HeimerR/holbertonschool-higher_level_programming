25ed22fabfd98d1039e4bf564b225667ec616601
Heimer <732@holbertonschool.com>
2019-06-11 19:31:55 +0000

    comment functions rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,22 +17,27 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ width"""
         return self.__width
 
     @property
     def height(self):
+        """height"""
         return self.__height
 
     @property
     def x(self):
+        """x"""
         return self.__x
 
     @property
     def y(self):
+        """y"""
         return self.__y
 
     @width.setter
     def width(self, value):
+        """width"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -41,6 +46,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        """height"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -49,6 +55,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        """x"""
         if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -57,6 +64,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        """y"""
         if type(value) != int:
             raise TypeError("y must be an integer")
         if value < 0:
@@ -64,13 +72,16 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """area"""
         return self.__width * self.__height
 
     def display(self):
+        """display"""
         print('\n' * self.__y + (" " * self.__x +
               (('#' * self.__width) + '\n')) * self.__height, end='')
 
     def __str__(self):
+        """str"""
         str1 = ("[{}] ({:d}) {:d}/{:d} - {:d}".format
                 (type(self).__name__, self.id, self.__x,
                  self.__y, self.__width))
@@ -79,6 +90,7 @@ class Rectangle(Base):
         return str1
 
     def update(self, *args, **kwargs):
+        """update"""
         attrs = [self.id, self.__width, self.__height, self.__x, self.__y]
         var = ('id', 'width', 'height', 'x', 'y')
         if kwargs is not None and (args is None or len(args) is 0):
@@ -93,6 +105,7 @@ class Rectangle(Base):
              self.__x, self.__y) = args_aux2
 
     def to_dictionary(self):
+        """to dictionary"""
         new_dict = {'id': self.id,
                     'width': self.width,
                     'height': self.height,
