cb704dafde6c4159cd87b7fdb3093f7d3ddc02ec
heimerr <732@holbertonschool.com>
2019-08-20 18:35:17 +0000

    case len = 3 added

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,6 +10,16 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 2:
         return (list_of_integers[0] if list_of_integers[0] >
                 list_of_integers[1] else list_of_integers[1])
+    if len(list_of_integers) == 3:
+        a = list_of_integers[0]
+        b = list_of_integers[1]
+        c = list_of_integers[2]
+        if a >= b and a >= c:
+            return a
+        elif b >= a and b >= c:
+            return b
+        else:
+            return c
     left = find_peak(list_of_integers[0:int(len(list_of_integers) / 2)])
     right = find_peak(list_of_integers[int(len(list_of_integers) / 2):])
     return left if left > right else right
