43ce38c4b84191e09b7ea26b7470f5b8f6231d40
heimerr <732@holbertonschool.com>
2019-08-20 18:07:19 +0000

    peak with recursion

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,8 +3,13 @@
 
 
 def find_peak(list_of_integers):
-    peak = list_of_integers[0]
-    for n in list_of_integers:
-        if n > peak:
-            peak = n
-    return peak
+    if len(list_of_integers) == 0:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return (list_of_integers[0] if list_of_integers[0] >
+                list_of_integers[1] else list_of_integers[1])
+    left = find_peak(list_of_integers[0:int(len(list_of_integers) / 2)])
+    right = find_peak(list_of_integers[int(len(list_of_integers) / 2):])
+    return left if left > right else right
