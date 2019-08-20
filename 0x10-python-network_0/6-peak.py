3d3d013d8029232535ca3bf4281c267d8c8f2751
heimerr <732@holbertonschool.com>
2019-08-20 22:07:36 +0000

    task pep8 fixed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,23 +5,11 @@
 def find_peak2(numbers, size, start, end):
     middle = int(start + (end - start) / 2)
 
-    # Compare middle element with its
-    # neighbours (if neighbours exist)
-    if ((middle == 0 or numbers[middle - 1] <= numbers[middle]) and
+    if ((middle is 0 or numbers[middle - 1] <= numbers[middle]) and
        (middle == size - 1 or numbers[middle + 1] <= numbers[middle])):
         return numbers[middle]
-
-    # If middle element is not peak and
-    # its left neighbour is greater
-    # than it, then left half must
-    # have a peak element
     elif (middle > 0 and numbers[middle - 1] > numbers[middle]):
         return find_peak2(numbers, size, start, (middle - 1))
-
-    # If middle element is not peak and
-    # its right neighbour is greater
-    # than it, then right half must
-    # have a peak element
     else:
         return find_peak2(numbers, size, middle + 1, end)
 
