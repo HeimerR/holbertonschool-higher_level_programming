de890f4437c487b30eff69cfe39c07eca04a56d4
heimerr <732@holbertonschool.com>
2019-08-26 15:10:21 +0000

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" takes in a URL, sends a request to the URL and displays the value
+of the X-Request-Id
+variable found in the header of the response.
+"""
+import urllib.request
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        r = urllib.request.urlopen(argv[1])
+        print(r.read().decode("utf-8"))
+    except urllib.error.HTTPError as error:
+        print("Error code: {}".format(error.code))
