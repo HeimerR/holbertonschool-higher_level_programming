7a0c281f3a8fb0f91ea16612b033e9eac10156dd
heimerr <732@holbertonschool.com>
2019-08-26 17:25:36 +0000

    task 9 added

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     search = {'search': argv[1]}
     r = requests.get("https://swapi.co/api/people/", params=search)
     result_num = r.json().get('count')
+    print("Number of results: {}".format(result_num))
     result_list = r.json().get('results')
-    for res in result_list:
-        print(r[res].get('name'))
+    for res in range(0, len(result_list)):
+        print(result_list[res].get('name'))
