b7c2447bb4f0e06f8c387aa0a5ca8b16245a02f0
heimerr <732@holbertonschool.com>
2019-08-26 22:00:51 +0000

    task 102

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Star Wars fetch info  """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    search = {'search': argv[1], 'page': '1'}
+
+    def starwars(search):
+        r = requests.get("https://swapi.co/api/people/", params=search)
+        if search['page'] == '1':
+            result_num = r.json().get('count')
+            print("Number of results: {}".format(result_num))
+        result_list = r.json().get('results')
+        for res in range(0, len(result_list)):
+            print(result_list[res].get('name'))
+            for film in result_list[res].get('films'):
+                r2 = requests.get(film)
+                title = r2.json().get('title')
+                print("\t{}".format(title))
+        return r.json().get('next')
+    next = starwars(search)
+    while next is not None:
+        search['page'] = str(int(search['page']) + 1)
+        next = starwars(search)
