357772c91be2dc6cb3e7e6fe85839a30b6a7e4ae
heimerr <732@holbertonshool.com>
2019-09-17 20:35:42 +0000

    [fix] console.log to test deleted

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,6 @@ if (process.argv[2] === undefined || (process.argv[3] === undefined)) {
 } else {
   process.argv.shift();
   process.argv.shift();
-  console.log(process.argv);
   const array = process.argv.map(Number).sort((a, b) => a - b).reverse();
   console.log(array[1]);
 }
