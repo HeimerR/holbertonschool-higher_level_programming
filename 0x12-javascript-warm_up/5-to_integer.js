213b1434dca0767f225b7c8c9a842dfe9400dab9
heimerr <732@holbertonschool.com>
2019-09-17 17:46:11 +0000

    [fix] Number.isNaN() changed to isNaN()

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 let n = parseInt(process.argv[2]);
-if (Number.isNaN(n) === true) {
+if (isNaN(n) === true) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + n);
