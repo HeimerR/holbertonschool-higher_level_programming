895e9f46cb537053e6fc6a3d6c548293b2e73704
heimerr <732@holbertonschool.com>
2019-09-17 18:13:39 +0000

    [fix] let statement deleted

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,8 +2,7 @@
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
 } else if (parseInt(process.argv[2]) > 0) {
-  let str = 'X'.repeat(parseInt(process.argv[2]));
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log(str);
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
 }
