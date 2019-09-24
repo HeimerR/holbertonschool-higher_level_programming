99e0e913067e393ae5d71c3feb87069f055cecec
heimerr <732@holbertonschool.com>
2019-09-24 20:26:28 +0000

    task 101 added sync await and promises used

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,20 +2,29 @@
 const id = process.argv[2];
 const url = 'http://swapi.co/api/films/' + id;
 const request = require('request');
-request(url, function getList(err, response, body) {
+function retrive (urlChar) {
+  return new Promise(function (resolve, reject) {
+    request(urlChar, function getChar (err2, response2, body2) {
+      if (err2) {
+        reject(err2);
+      } else {
+        resolve(JSON.parse(body2).name);
+      }
+    });
+  });
+}
+async function getlist (urlist) {
+  for (const urlChar of urlist) {
+    const character = await retrive(urlChar);
+    console.log(character);
+  }
+}
+
+request(url, function getList (err, response, body) {
   if (err) {
     throw err;
   } else {
-    list = [];
-    JSON.parse(body).characters.forEach( async function (urlChar) {
-      let res = await request(urlChar, function getChar(err2, response2, body2) {
-        if (err2) {
-          throw err2;
-        } else {
-          list.push(JSON.parse(body2).name);
-        }
-      });
-    });
-    console.log(list);
+    const urlist = JSON.parse(body).characters;
+    getlist(urlist);
   }
 });
