640a9f75caefaa42b1ee1379f924792e2f0bb1e8
heimerr <732@holbertonschool.com>
2019-09-24 17:10:43 +0000

    task 0 fixed console.error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const file = process.argv[2];
 const fs = require('fs');
 fs.readFile(file, 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
     process.stdout.write(data);
   }
