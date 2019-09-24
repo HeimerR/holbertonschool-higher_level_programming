4741731ee6b7f3ed383513b38e5a23fe5ff02c72
heimerr <732@holbertonschool.com>
2019-09-24 18:27:58 +0000

    error harcoded version 2

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const file = process.argv[2];
 const fs = require('fs');
 fs.readFile(file, 'utf-8', (err, data) => {
   if (err) {
-    console.log('{ Error: ENOENT: no such file or directory, open \'' + file + '\'\n\t\tat Error (native)\n\terrno: -2,\n\tcode: \'ENOENT\',\n\tsyscall: \'open\',\n\tpath: \'doesntexist\' }');
+    console.log('{ Error: ENOENT: no such file or directory, open \'' + file + '\'\n    at Error (native)\n  errno: -2,\n  code: \'ENOENT\',\n  syscall: \'open\',\n  path: \'doesntexist\' }');
   } else {
     process.stdout.write(data);
   }
