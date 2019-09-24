82726e19e0bc9b3655e48be3fe88c348997b47a2
heimerr <732@holbertonschool.com>
2019-09-24 22:29:56 +0000

    task 0 rollback checker issue

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const file = process.argv[2];
 const fs = require('fs');
 fs.readFile(file, 'utf-8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     process.stdout.write(data);
   }
