b9f0e652b7f95ed5835e666d191079c5dc85b819
heimerr <732@holbertonschool.com>
2019-09-24 17:58:50 +0000

    console.log changed to console.error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,14 @@ const url = process.argv[2];
 const request = require('request');
 request(url, function (err, response, body) {
   if (err) {
-    console.log(err);
-  } else {
-    let list = [];
-    for (const film of JSON.parse(body).results) {
-      list = list.concat(film.characters);
-    }
-    // console.log(list);
-    const uniq = list.filter(x => x === 'https://swapi.co/api/people/18/');
-    console.log(uniq.length);
+    console.error(err);
   }
+  let list = [];
+  const result = JSON.parse(body).results;
+  for (const film of result) {
+    list = list.concat(film.characters);
+  }
+  const uniq = list.filter(x => x === 'https://swapi.co/api/people/18/');
+  console.log(uniq.length);
 }
 );
