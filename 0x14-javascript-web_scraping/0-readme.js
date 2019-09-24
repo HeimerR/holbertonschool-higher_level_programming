3acd181cae6d8789f76a9e33b12589c7ffa251ad
heimerr <732@holbertonschool.com>
2019-09-24 22:27:43 +0000

    task 0 rollback checker issue

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const file = process.argv[2];
 const fs = require('fs');
 fs.readFile(file, 'utf-8', (err, data) => {
   if (err) {
-    console.log('{ Error: ENOENT: no such file or directory, open \'' + file + '\'\n    at Error (native)\n  errno: -2,\n  code: \'ENOENT\',\n  syscall: \'open\',\n  path: \'doesntexist\' }');
+    console.error(err);
   } else {
     process.stdout.write(data);
   }
