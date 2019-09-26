f0f03f6652430388f7d1562d970f1d9e3a4ef835
heimerr <732@holbertonschool.com>
2019-09-26 17:04:25 +0000

    semistandar passed

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  $("DIV#add_item").click(function(){
-    $("UL.my_list").append('<li>Item</li>');
+$(document).ready(function () {
+  $('DIV#add_item').click(function () {
+    $('UL.my_list').append('<li>Item</li>');
   });
 });
--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  $("DIV#update_header").click(function(){
-    $("HEADER").text("New Header!!!");
+$(document).ready(function () {
+  $('DIV#update_header').click(function () {
+    $('HEADER').text('New Header!!!');
   });
 });
