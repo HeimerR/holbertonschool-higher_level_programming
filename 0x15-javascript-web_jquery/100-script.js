3001334c85cf2b63ba503e75319eb5cff41fe82e
heimerr <732@holbertonschool.com>
2019-09-27 03:28:11 +0000

    task 100 semistandar passed

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
   const headerElem = document.querySelector('HEADER');
-  headerElem.style.color = "#FF0000";
+  headerElem.style.color = '#FF0000';
 });
